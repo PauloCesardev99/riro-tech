a9249e6f70ef57d18e6024ae1997c7ba036808eb
PauloCesardev99 <paulocesarbritocamilo@gmail.com>
2024-01-17 20:49:29 -0300

    Versão Atualizada

---
+++ src/script/index.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const menu = window.document.getElementById("menu")
+const links_menu = window.document.getElementById("links_menu")
+const botao_form = window.document.getElementById("enviar")
+
+//Menu Hamburg
+
+menu.addEventListener("click",()=>{
+    links_menu.classList.toggle("desbloquear-menu") 
+})
+
+
+//Enviar form
+
+
+
